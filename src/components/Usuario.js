ce47cff084467342ee1c22166734998e1c6b6f06
AiKurogaya <yujikuroda12@hotmail.com>
2022-09-19 07:24:36 -0300

    feat: implemented 'MudarNome' function

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,12 +1,27 @@
+import React from 'react';
+
 export default function Usuario(props) {
+
+    const usuario = props.name
+
+    const [nome, setNome] = React.useState(usuario);
+
+    function mudarNome() {
+        const newUser = prompt("Qual o novo nome de usuário?");
+        console.log(newUser);
+        if (newUser !== '') {
+            setNome(newUser);
+        }
+    }
+
     return (
         <div class="usuario">
             <img src={props.img} alt={props.user} />
             <div class="texto">
                 <strong>{props.user}</strong>
                 <span>
-                {props.name}
-                <ion-icon name="pencil"></ion-icon>
+                {nome}
+                <ion-icon name="pencil" onClick={mudarNome}></ion-icon>
                 </span>
             </div>
         </div>
